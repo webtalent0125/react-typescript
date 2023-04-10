c3233f4179fe0c747cec573210a7a4ec8e5dadfc
webtalent0125 <talenteddev0125@gmail.com>
2023-04-10 14:22:47 +0900

    project edit

---
+++ src/projects/ProjectsPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { MOCK_PROJECTS } from './MockProjects';
+import ProjectList from './ProjectList';
+import { Project } from './Project'
+
+function ProjectsPage() {
+    const saveProject = (project: Project) => {
+        console.log('Saving project: ', project);
+    };
+    return <>
+        <h1>Projects</h1>
+        <ProjectList onSave={saveProject} projects={MOCK_PROJECTS} />
+    </>;
+}
+
+export default ProjectsPage;
